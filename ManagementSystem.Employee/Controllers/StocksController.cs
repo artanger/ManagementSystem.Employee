d5ce3c05e0a25127d1756e3b21bcb08c6ac69350
artanger <artimag666@gmail.com>
2021-02-03 23:52:33 +0200

    Added ProductDetailsController and StocksController scaffolding

---
+++ ManagementSystem.Employee/Controllers/StocksController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ManagementSystem.Employee.Models;
+
+namespace ManagementSystem.Employee.Controllers
+{
+    public class StocksController : Controller
+    {
+        private InternetShopDBEntities db = new InternetShopDBEntities();
+
+        // GET: Stocks
+        public ActionResult Index()
+        {
+            var stocks = db.Stocks.Include(s => s.Products);
+            return View(stocks.ToList());
+        }
+
+        // GET: Stocks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stocks stocks = db.Stocks.Find(id);
+            if (stocks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stocks);
+        }
+
+        // GET: Stocks/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "Name");
+            return View();
+        }
+
+        // POST: Stocks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,Qty,ID")] Stocks stocks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stocks.Add(stocks);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "Name", stocks.ProductID);
+            return View(stocks);
+        }
+
+        // GET: Stocks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stocks stocks = db.Stocks.Find(id);
+            if (stocks == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "Name", stocks.ProductID);
+            return View(stocks);
+        }
+
+        // POST: Stocks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,Qty,ID")] Stocks stocks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stocks).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "Name", stocks.ProductID);
+            return View(stocks);
+        }
+
+        // GET: Stocks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stocks stocks = db.Stocks.Find(id);
+            if (stocks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stocks);
+        }
+
+        // POST: Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Stocks stocks = db.Stocks.Find(id);
+            db.Stocks.Remove(stocks);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
